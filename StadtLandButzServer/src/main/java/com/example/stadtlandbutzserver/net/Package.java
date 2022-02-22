1ffb2732dabfc69e01ce146f768d1e0afead87b0
Medox36 <giuntinil@bzz.ch>
2022-02-22 22:25:44 +0100

    updated Package.java to contain a key String

--- StadtLandButzServer/src/main/java/com/example/stadtlandbutzserver/net/Package.java
+++ StadtLandButzServer/src/main/java/com/example/stadtlandbutzserver/net/Package.java
@@ -5,9 +5,11 @@ import java.io.Serializable;
 public class Package implements Serializable {
     String prefix;
     String information;
+    String id;
 
-    public Package(String prefix, String information) {
+    public Package(String prefix, String information, String id) {
         this.prefix = prefix;
         this.information = information;
+        this.id = id;
     }
 }
