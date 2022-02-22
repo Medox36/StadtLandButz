37cb95d243ec12710d0566e54ad748396465558c
Medox36 <giuntinil@bzz.ch>
2022-02-22 22:59:22 +0100

    changed the id from type String to type UUID

--- StadtLandButzClient/src/main/java/com/example/stadtlandbutzclient/net/Package.java
+++ StadtLandButzClient/src/main/java/com/example/stadtlandbutzclient/net/Package.java
@@ -1,13 +1,14 @@
 package com.example.stadtlandbutzclient.net;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class Package implements Serializable {
     String prefix;
     String information;
-    String id;
+    UUID id;
 
-    public Package(String prefix, String information, String id) {
+    public Package(String prefix, String information, UUID id) {
         this.prefix = prefix;
         this.information = information;
         this.id = id;
