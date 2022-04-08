780de6cb9a27ad298cb6176134e66f1b839c8ca9
Medox36 <giuntinil@bzz.ch>
2022-04-08 17:36:19 +0200

    started to implement methods and classes in StadtLandButz_ClientHostServer

---
+++ StadtLandButz_ClientHostServer/StadtLandButz_Server/src/main/java/com/example/stadtlandbutz_server/net/Host.java
@@ -0,0 +1,22 @@
+package com.example.stadtlandbutz_server.net;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Host {
+    private Socket socket;
+    private SenderThread senderThread;
+    private ReceiverThread receiverThread;
+
+    public Host(Socket socket) throws IOException {
+        this.socket = socket;
+        senderThread = new SenderThread(socket.getOutputStream());
+        senderThread.start();
+        receiverThread = new ReceiverThread(socket.getInputStream());
+        receiverThread.start();
+    }
+
+    public synchronized void sendPackage(Package p) {
+        senderThread.addPackageToSendStack(p);
+    }
+}
---
+++ StadtLandButz_ClientHostServer/StadtLandButz_Server/src/main/java/com/example/stadtlandbutz_server/net/connection/ClientConnection.java
@@ -0,0 +1,64 @@
+package com.example.stadtlandbutz_server.net.connection;
+
+import com.example.stadtlandbutz_server.game.Game;
+import com.example.stadtlandbutz_server.net.Client;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ClientConnection extends Thread {
+    private boolean await;
+    private ServerSocket serverSocket;
+
+    public ClientConnection() {
+        try {
+            serverSocket = new ServerSocket(24452, 5);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        await = true;
+        await();
+    }
+
+    private void await() {
+        try {
+            serverSocket.setSoTimeout(30000);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        do {
+            try {
+                Socket socket = serverSocket.accept();
+                Game.addClient(socket);
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    //client.sendPackage(new Package("0000", "", null));
+                }).start();
+            } catch (IOException e) {
+                //e.printStackTrace();
+            }
+        } while (await);
+    }
+
+    public void letClientsConnect(boolean await) {
+        this.await = await;
+    }
+
+    public void exit() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
