0819c63449768013a341f77da24a2e56337ce11a
Medox36 <giuntinil@bzz.ch>
2023-02-11 17:22:56 +0100

    [StadtLandButz_Host & StadtlandButz_Client] removed unused dependency org.openjfx.javafx-fxml

--- StadtLandButz_ClientHostServer/StadtLandButz_Client/src/main/java/module-info.java
+++ StadtLandButz_ClientHostServer/StadtLandButz_Client/src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module ch.giuntini.stadtlandbutz_client {
     requires ch.giuntini.stadtlandbutz_package;
     requires javafx.controls;
-    requires javafx.fxml;
 
-    opens ch.giuntini.stadtlandbutz_client.gui to javafx.fxml;
+    opens ch.giuntini.stadtlandbutz_client.gui to javafx.controls;
     exports ch.giuntini.stadtlandbutz_client.game;
     exports ch.giuntini.stadtlandbutz_client.net;
     exports ch.giuntini.stadtlandbutz_client.gui;
--- StadtLandButz_ClientHostServer/StadtLandButz_Host/src/main/java/module-info.java
+++ StadtLandButz_ClientHostServer/StadtLandButz_Host/src/main/java/module-info.java
@@ -2,9 +2,8 @@ module ch.giuntini.stadtlandbutz_host {
     requires ch.giuntini.stadtlandbutz_package;
     requires string.similarity;
     requires javafx.controls;
-    requires javafx.fxml;
 
-    opens ch.giuntini.stadtlandbutz_host.gui to javafx.fxml;
+    opens ch.giuntini.stadtlandbutz_host.gui to javafx.controls;
     exports ch.giuntini.stadtlandbutz_host.game;
     exports ch.giuntini.stadtlandbutz_host.net;
     exports ch.giuntini.stadtlandbutz_host.gui;
